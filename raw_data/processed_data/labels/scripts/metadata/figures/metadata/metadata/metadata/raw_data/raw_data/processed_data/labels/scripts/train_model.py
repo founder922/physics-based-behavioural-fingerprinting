5176e4920be4089b2ee77f4f444454163101e68c
founder922 <founder@technopilot.in>
2026-03-22 16:31:48 +0530

    Add RandomForest model training script

--- raw_data/processed_data/labels/scripts/metadata/figures/metadata/metadata/metadata/raw_data/raw_data/processed_data/labels/scripts/train_model.py
+++ raw_data/processed_data/labels/scripts/metadata/figures/metadata/metadata/metadata/raw_data/raw_data/processed_data/labels/scripts/train_model.py
@@ -1,1 +1,16 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
+data = pd.read_csv("../processed_data/features.csv")
+
+X = data.drop(["segment_id", "label"], axis=1)
+y = data["label"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+model = RandomForestClassifier(n_estimators=100, max_depth=10)
+model.fit(X_train, y_train)
+
+accuracy = model.score(X_test, y_test)
+print("Model accuracy:", accuracy)
